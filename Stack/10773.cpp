291bbfaeb2d8e43d3ea06578e886ca65ed6c57d8
Seunghui-Shin <83438707+Seunghui-Shin@users.noreply.github.com>
2022-03-07 23:38:28 +0900

    Create 10773.cpp

---
+++ Stack/10773.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+int n, k;
+stack<int> st;
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n;
+    for (int i = 0;i < n;i++) {
+        cin >> k;
+        if (k == 0) {
+            st.pop();
+        }
+        else {
+            st.push(k);
+        }
+    }
+    int sum = 0;
+    while (!st.empty()) {
+        sum += st.top();
+        st.pop();
+    }
+    cout << sum;
+    return 0;
+}
